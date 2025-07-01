4c4cc02c2f62e153ca4d136581b761df036d4d84
Carsten Lund <clu@rts.dk>
2025-07-01 11:15:28 +0200

    Update script.js
    simplere javacript

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("body").addEventListener("click", function () {
-    document.querySelector("body").classList.add("animate__tada");
-    setTimeout(() => {
-      document.querySelector("body").classList.remove("animate__tada");
+  document.body.addEventListener("click", function () {
+    document.body.classList.add("animate__tada");
+    
+    setTimeout(function() {
+      document.body.classList.remove("animate__tada");
     }, 500);
   });
-});
