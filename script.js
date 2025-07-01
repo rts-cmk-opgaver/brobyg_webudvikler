a123fceff33b5df5bc96c7e8c3b47372c0af12b8
Carsten Lund <clu@rts.dk>
2025-07-01 11:25:20 +0200

    Update script.js
    tilføjet line breaks

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
   document.body.addEventListener("click", function () {
+    
     document.body.classList.add("animate__tada");
     
     setTimeout(function() {
       document.body.classList.remove("animate__tada");
     }, 500);
+
   });
